563c0867885afb4000a33ceb5896238a1ebca4c5
Nahoov <newnahomi15@gmail.com>
2025-05-28 15:34:40 -0300

    alteración en la conexion de banco

--- paginas/conexao.php
+++ paginas/conexao.php
@@ -3,9 +3,9 @@
 define('HOST','database-churros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com');
 define('USER','admin');
 define('PASS','cadastrochurros');
-define('BASE','database-churros');
+define('BASE','`database-churros`');
 
-$conn = new mysqli(HOST,USER,PASS,BASE);
+$conn = new mysqli('database-churros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com',USER,PASS,BASE);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

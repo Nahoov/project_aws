d55c29e4c7c865175c780012f91708530f29278a
Nahoov <newnahomi15@gmail.com>
2025-05-30 12:59:36 -0300

    Cambio de bd

--- paginas/conexao.php
+++ paginas/conexao.php
@@ -1,11 +1,11 @@
 <?php
 
-define('HOST','database-churros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com');
+define('HOST','databasechurros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com');
 define('USER','admin');
 define('PASS','cadastrochurros');
-define('BASE','`database-churros`');
+define('BASE','databasechurros');
 
-$conn = new mysqli('database-churros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com',USER,PASS,BASE);
+$conn = new mysqli('databasechurros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com',USER,PASS,BASE);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

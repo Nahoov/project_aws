51a5e83c0c20bee0ac32e2683f8a2a5cf33f6f14
Nahoov <newnahomi15@gmail.com>
2025-05-31 16:00:04 -0300

    conexion mysql

--- paginas/conexao.php
+++ paginas/conexao.php
@@ -5,7 +5,7 @@ define('USER','admin');
 define('PASS','cadastrochurros');
 define('BASE','databasechurros');
 
-$conn = new mysqli('databasechurros.c5sgwwsm4w2a.sa-east-1.rds.amazonaws.com',USER,PASS,BASE);
+$conn = new mysqli(HOST,USER,PASS,BASE);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
